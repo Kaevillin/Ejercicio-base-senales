5dc67c15ca00f1de6dd654dc0b12a91382ebace6
kevinMG01 <Kaevillin@gmail.com>
2022-11-22 23:25:06 -0300

    señales
    segundo intento de señales

--- ejercicio_senales/scenes/Buttons/RedGenerator/RedGenerator.gd
+++ ejercicio_senales/scenes/Buttons/RedGenerator/RedGenerator.gd
@@ -1,2 +1,13 @@
 extends TextureButton
 var bloqueVioleta = preload("res://scenes/Blocks/Red/BlockRed.tscn")
+
+
+func _on_RedGenerator_button_down():
+	generate_block()
+	pass 
+
+
+func generate_block():
+	var instancia_bloqueVioleta = bloqueVioleta.instance()
+	add_child(instancia_bloqueVioleta)
+	pass
--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -1,14 +1,28 @@
 extends Node2D
 
+
 const INSTANCES_TEXT = "Cantidad de instancias: "
 # Variable para contar cantidad de instancias
 var instancesCount = 0
 
+func count_new_instance():
+	instancesCount += 1
+	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
+	pass
+
 func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
+	$RedGenerator.connect("button_down" , self, "count_new_intance")
+	$VioletGenerator.connect("button_down" , self, "count_new_intance")
 	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
 	pass
+	
+func delete():
+	instancesCount = 0
+	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
+	pass
+
 
 
 func _on_RedGenerator_button_down():
